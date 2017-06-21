d891922520752cbe85249879a582812c5865cb4d
guillaumebarranco <guillaume.barranco8@hotmail.fr>
2017-06-21 12:06:41 +0200

    [IMPROV] Make work tweets script

--- tweets.js
+++ tweets.js
@@ -1,12 +1,16 @@
-var Twit = require('twit')
+var Twit = require('twit');
+
+var config = require('./config.json').twitter;
+
+console.log(config);
 
 var T = new Twit({
-	  consumer_key:         '...',
-	  consumer_secret:      '...',
-	  access_token:         '...',
-	  access_token_secret:  '...',
+	  consumer_key:         config.consumer_key,
+	  consumer_secret:      config.consumer_secret,
+	  access_token:         config.access_token,
+	  access_token_secret:  config.access_token_secret,
 	  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-})
+});
 
 
 T.get('search/tweets', {
@@ -16,5 +20,5 @@ T.get('search/tweets', {
 
 }, function(err, data, response) {
 	console.log(data);
-}
+});
 
